e39b1cf8b6db747ea57ce8570f372ec22ed74580
Patrick Drechsler <patrick.drechsler@WFUE530548>
2020-03-10 11:21:23 +0100

    initial commit

---
+++ DemoWareHouseApi/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:DemoWareHouseApi.Controllers.WeatherForecastController.Get~System.Collections.Generic.IEnumerable{DemoWareHouseApi.WeatherForecast}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "<Pending>", Scope = "type", Target = "~T:DemoWareHouseApi.Program")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:DemoWareHouseApi.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder,Microsoft.AspNetCore.Hosting.IWebHostEnvironment)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:DemoWareHouseApi.Startup.ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection)")]
