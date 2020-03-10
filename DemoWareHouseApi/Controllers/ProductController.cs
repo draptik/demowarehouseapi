797c260d8b8bfd5805e396a815785c58dbfee8b2
Patrick Drechsler <patrick.drechser@redheads.de>
2020-03-10 16:48:42 +0100

    add product controller

---
+++ DemoWareHouseApi/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using DemoWareHouseApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWareHouseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/Product
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product { Id = 1, Name = "test1", Price = 10 },
+                new Product { Id = 2, Name = "test2", Price = 11 },
+                new Product { Id = 3, Name = "test3", Price = 12 },
+                new Product { Id = 4, Name = "test4", Price = 13 },
+                new Product { Id = 5, Name = "test5", Price = 14 }
+            };
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public Product Get(int id)
+        {
+            return new Product { Id = id, Name = $"test{id}", Price = id * 10 };
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] Product product)
+        {
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product product)
+        {
+        }
+
+        // DELETE: api/Product/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoWareHouseApi/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoWareHouseApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
