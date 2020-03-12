e658cca212aa9fb0ecf14c405b358b4e1f5b84b7
Patrick Drechsler <patrick.drechser@redheads.de>
2020-03-12 11:02:46 +0100

    remove comments

--- DemoWareHouseApi/Startup.cs
+++ DemoWareHouseApi/Startup.cs
@@ -17,7 +17,6 @@ namespace DemoWareHouseApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // Authentication
@@ -34,7 +33,6 @@ namespace DemoWareHouseApi
             services.AddCustomSwagger();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
